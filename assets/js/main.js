f20bb0f6e9da7924a5aeb0b6040fd3f359ee912d
kotadias58 <kotadias58@gmail.com>
2018-06-24 16:58:07 -0300

    $1

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,11 @@ $(document).ready( function() {
 		let top = $(window).scrollTop();
 
 		if (top >= wheight) {
-			$(".navbar-dark").removeClass("bg-transparent");
-			$(".navbar-dark").addClass("bg-dark");
-			$(".main-menu").css("background-color", "rgba(0,0,0, 1)");
+			$(".navbar-dark, .main-menu").removeClass("bg-transparent");
+			$(".navbar-dark, .main-menu").addClass("bg-dark");
 		} else {
-			$(".navbar-dark").removeClass("bg-dark");
-			$(".navbar-dark").addClass("bg-transparent");
-			$(".main-menu").css("background-color", "rgba(0,0,0, 0)");
+			$(".navbar-dark, .main-manu").removeClass("bg-dark");
+			$(".navbar-darkm ,main-menu").addClass("bg-transparent");
 		}
 	});
 
