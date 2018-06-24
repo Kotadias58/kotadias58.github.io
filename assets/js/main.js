72647b58e6858d48e5e4bfaff7a51a118e0ff9fe
kotadias58 <kotadias58@gmail.com>
2018-06-24 20:17:44 -0300

    $1

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,9 @@ $(document).ready( function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+      alert("TARGET: " + target);
+      
       if (target.length) {
         $('html, body').animate({
           scrollTop: (target.offset().top - 54)
