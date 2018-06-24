4388db27e490b4bbbf3a9c8657e4879d9e517afd
kotadias58 <kotadias58@gmail.com>
2018-06-24 09:59:30 -0300

    $1

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,10 @@ $(document).ready( function() {
 		//alert("Height: " + wheight + " - Top: " + top); 
 
 		if (top >= wheight) {
-			$(".bg-dark").css("background-color", "rgba(255,255,255, 1)");
+			$(".bg-dark").css("background-color", "rgba(255,255,255, 1) !important");
 			alert("Top é maior ou igual a Height " + wheight + " - " +top); 
 		} else {
-			$(".bg-dark").css("background-color", "rgba(255,255,255, 0)");
+			$(".bg-dark").css("background-color", "rgba(255,255,255, 0) !important");
 			alert("Top é menos que Height: " + wheight + " - " +top); 
 		}
 	});
