cb6751d458cee7b8e57cfc73f89e0542bd009c63
kotadias58 <kotadias58@gmail.com>
2018-06-24 20:13:13 -0300

    $1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
 $(document).ready( function() {
-    "use strict"; // Start of use strict
-
 	let wheight = $(window).height();
 
 	$(window).on("scroll", function() {
@@ -15,33 +13,8 @@ $(document).ready( function() {
 		}
 	});
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-	    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-	      if (target.length) {
-	        $('html, body').animate({
-	          scrollTop: (target.offset().top - 54)
-	        }, 1000, "easeInOutExpo");
-	        return false;
-	      }
-	    }
-    });
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-    	$('.navbar-collapse').collapse('hide');
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-    	target: '#navbar-fixa',
-    	offset: 54
-  	});
 });
 
-/*
 (function($) {
   "use strict"; // Start of use strict
 
@@ -66,7 +39,7 @@ $(document).ready( function() {
 
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
-    target: '#navbar-fixed',
+    target: '#navbar-fixa',
     offset: 54
   });
 
