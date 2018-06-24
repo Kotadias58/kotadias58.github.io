24159707fae0f91e88374a2e84a001d262612e79
kotadias58 <kotadias58@gmail.com>
2018-06-24 16:53:45 -0300

    $1

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,11 @@ $(document).ready( function() {
 		if (top >= wheight) {
 			$(".navbar-dark").removeClass("bg-transparent");
 			$(".navbar-dark").addClass("bg-dark");
+			$(".main-menu").css("background-color", "rgba(0,0,0, 1)");
 		} else {
 			$(".navbar-dark").removeClass("bg-dark");
 			$(".navbar-dark").addClass("bg-transparent");
+			$(".main-menu").css("background-color", "rgba(0,0,0, 0)");
 		}
 	});
 
