c57b8f7bcd13f7d2eb21d956724f440c714515e5
kotadias58 <kotadias58@gmail.com>
2018-06-24 10:11:57 -0300

    $1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,13 @@ $(document).ready( function() {
 
 	$(window).on("scroll", function() {
 		let top = $(window).scrollTop();
-		//alert("Height: " + wheight + " - Top: " + top); 
 
 		if (top >= wheight) {
 			$(".navbar-dark").removeClass("bg-transparent");
 			$(".navbar-dark").addClass("bg-dark");
-			alert("Top é maior ou igual a Height " + wheight + " - " +top); 
 		} else {
 			$(".navbar-dark").removeClass("bg-dark");
 			$(".navbar-dark").addClass("bg-transparent");
-			alert("Top é menos que Height: " + wheight + " - " +top); 
 		}
 	});
 
