4a7f1f9b686689b9a21b490f8c2f80d16d4b65cd
kotadias58 <kotadias58@gmail.com>
2018-06-23 22:36:50 -0300

    $1

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready( function() {
+	var wheight = $(window).height();
+
+	$(window).scroll(function() {
+		var top = $(window).scrollTop(); 
+
+		if ( top >= wheight) {
+			$("#navbar-fixa").css("background-color", "rgba(255,255,255, 1)");
+
+		} else {
+			$("#navbar-fixa").css("background-color", "rgba(255,255,255, 0)");
+		}
+	});
+
+});
+
