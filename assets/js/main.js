d8d99bf277f24e6951c72f97afb4d12063c66ca6
kotadias58 <kotadias58@gmail.com>
2018-06-24 09:53:51 -0300

    $1

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,10 @@ $(document).ready( function() {
 
 		if (top >= wheight) {
 			$(".bg-dark").css("background-color", "rgba(255,255,255, 1)");
+			alert("Height: " + wheight + " - Top: " + top); 
 		} else {
 			$(".bg-dark").css("background-color", "rgba(255,255,255, 0)");
+			alert("Height: " + wheight + " - Top: " + top); 
 		}
 	});
 
