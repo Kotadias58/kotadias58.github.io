59e30b90cdf8d0cb9586e20bd701cbdc21881f30
kotadias58 <kotadias58@gmail.com>
2018-06-26 15:14:46 -0300

    $1

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,94 +22,49 @@ $(document).ready( function() {
 
 	baguetteBox.run('.cards-gallery', { animation: 'slideIn'});
 
-
-	/*
-
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-	    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-	      	var target = $(this.hash);
-	      	target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-	      	if (target.length) {
-	        	$('html, body').animate({
-	          		scrollTop: (target.offset().top - 54)
-	        	}, 1000, "easeInOutExpo");
-	        	return false;
-	      	}
-	    }
-    });
-
-  	// Closes responsive menu when a scroll trigger link is clicked
-  	$('.js-scroll-trigger').click(function() {
-    	$('.navbar-collapse').collapse('hide');
-  	});
-
-  	// Activate scrollspy to add active class to navbar items on scroll
-  	$('body').scrollspy({
-    	target: '#navbar-fixa',
-    	offset: 54
-  	});
-	*/
-
-	/*
-    //Captura cada elemento div com o data-type "background"
-    $('div[data-type="background"]').each(function(){
-        var $scroll = $(this);
-
-        //Captura o evento scroll do navegador e modifica o backgroundPosition de acordo com seu deslocamento.            
-        $(window).scroll(function() {
-            var yPos = -($(window).scrollTop() / $scroll.data('speed')); 
-            var coords = '50% '+ yPos + 'px';
-            $scroll.css({ backgroundPosition: coords });    
-        });
-    });
-    */  
-
-    /*
-    $('section[data-type="background"]').each(function(){
-        var $scroll = $(this);   
-            //Captura o evento scroll do navegador e modifica o backgroundPosition de acordo com seu deslocamento.            
-            $(window).scroll(function() {
-                var yPos = -($(window).scrollTop() / $scroll.data('speed')); 
-                var coords = '50% '+ yPos + 'px';
-                $scroll.css({ backgroundPosition: coords });    
-            });
-    });
-    */
-          
 });
 
-/*
 (function($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-	    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-	      	var target = $(this.hash);
-	      	target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
 
-	      	if (target.length) {
-	        	$('html, body').animate({
-	          		scrollTop: (target.offset().top - 54)
-	        	}, 1000, "easeInOutExpo");
-	        	return false;
-	      	}
-	    }
-    });
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
 
-  	// Closes responsive menu when a scroll trigger link is clicked
-  	$('.js-scroll-trigger').click(function() {
-    	$('.navbar-collapse').collapse('hide');
-  	});
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
 
-  	// Activate scrollspy to add active class to navbar items on scroll
-  	$('body').scrollspy({
-    	target: '#navbar-fixa',
-    	offset: 54
-  	});
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
-*/
 
 
